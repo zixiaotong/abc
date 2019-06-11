d2e0d48146abaa41c879dc85247fb5454d75e63b
shanglei <shanglei123>
2019-06-11 23:09:29 +0800

    commit

--- src/main/java/com/abc/abc/controller/IndexController.java
+++ src/main/java/com/abc/abc/controller/IndexController.java
@@ -1,11 +1,21 @@
 package com.abc.abc.controller;
 
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,11 +47,60 @@ public class IndexController {
 
     /**
      * 为请求里传的参数 占位
-     *
+     * 访问时 http://localhost:8081/index5/hello
+     * 返回：indexhello
      * PathVariable
      */
     @GetMapping(path = {"/index5/{message}"})
-    public String index1(@PathVariable String message) {
+    public String index5(@PathVariable String message) {
         return "index" + message + "";
+    }
+
+    /**
+     * 这样访问：http://localhost:8081/index6?message=4
+     * 返回：index：4
+     * 参数为空会报错：Required String parameter 'message' is not present
+     *
+     * @param message
+     */
+    @GetMapping(path = {"/index6"})
+    public String index6(@RequestParam String message) {
+        return "index：" + message + "";
+    }
+
+    /**
+     * required = false 加false 代表参数可以为空
+     * value = "m" 参数为字段为m，message不可用
+     * defaultValue = "Empty" 默认不传就是Empty
+     *
+     * @param message
+     */
+    @GetMapping(path = {"/index7"})
+    public String index7(@RequestParam(value = "m", required = false, defaultValue = "Empty") String message,
+                         HttpServletRequest request,
+                         @RequestParam(value = "age", required = false, defaultValue = "Empty") Integer age) {
+        String message2 = request.getParameter("message2");
+        return "index：" + message + "" + "age=" + age;
+    }
+
+    /**
+     * Accept
+     */
+    @GetMapping(path = {"/header"})
+    public String header(@RequestHeader(value = "Accept") String acceptHeader) {
+        return "index：" + acceptHeader + "";
+    }
+
+    /**
+     * 往ResponseEntity里写值
+     *
+     * @param
+     */
+    @GetMapping(path = {"/htmlResponseEntity"})
+    public ResponseEntity<String> htmlResponseEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("a", Arrays.asList("aaa"));
+        ResponseEntity entity = new ResponseEntity("aaaa", headers, HttpStatus.OK);
+        return entity;
     }
 }
