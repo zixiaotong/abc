37d9daaffb749877cfaaf60cb21dd76f310708df
尚磊 <shanglei@gogoup.com>
2019-06-21 16:32:14 +0800

    init

--- src/main/java/com/abc/abc/AbcApplication.java
+++ src/main/java/com/abc/abc/AbcApplication.java
@@ -3,13 +3,22 @@ package com.abc.abc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author zishang
  */
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("com.abc.abc.dao.persistence")
-public class AbcApplication {
+public class AbcApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AbcApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AbcApplication.class, args);
