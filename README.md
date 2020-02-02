2994998ebb5a57c4ee7d562a36c7fd5357b18570
尚磊 <shanglei@gogoup.com>
2020-02-02 16:31:45 +0800

    7788

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
  * java -jar xxx.jar --server.port=8888
  * 连续两个减号--就是对application.properties 中的属性值进行赋值的标识。等价于在application.properties中添加属性server.port=8888
+ * java -jar xxx.jar --spring.profiles.active=dev 启动时来确定走哪个环境的配置
--- src/main/java/com/abc/abc/api/course/CourseApi.java
+++ src/main/java/com/abc/abc/api/course/CourseApi.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,32 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/CourseApi")
 public class CourseApi extends BaseController {
-    @Autowired
-    private CourseService courseService;
+	@Autowired
+	private CourseService courseService;
+	// @Autowired
+	// private CounterService counterService;
 
-    @RequestMapping(value = "/getCourseByCourseId/{id}", method = RequestMethod.GET)
-    @ApiOperation(value = "根据课程id获取课程数据", notes = "根据课程id获取课程数据")
-    @ApiImplicitParams({//请求参数描述
-            @ApiImplicitParam(name = "id", value = "课程ID", required = true, dataTypeClass = Long.class),
-    })
-    public GogoupCourse getCourseByUserId(@PathVariable("id") Long id) {
-        try {
-            this.log_info("根据课程id获取课程数据-课程ID:{}", new Object[]{id});
-            GogoupCourse user = courseService.getCourseByUserId(Long.valueOf(id));
-            return user;
-        } catch (Exception e) {
-            this.log_error("getCourseByUserId", e);
-        }
-        return null;
-    }
+	@RequestMapping(value = "/getCourseByCourseId/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "根据课程id获取课程数据", notes = "根据课程id获取课程数据")
+	@ApiImplicitParams({ // 请求参数描述
+			@ApiImplicitParam(name = "id", value = "课程ID", required = true, dataTypeClass = Long.class), })
+	public GogoupCourse getCourseByUserId(@PathVariable("id") Long id) {
+		try {
+			this.log_info("根据课程id获取课程数据-课程ID:{}", new Object[] { id });
+			GogoupCourse user = courseService.getCourseByUserId(Long.valueOf(id));
+			return user;
+		} catch (Exception e) {
+			this.log_error("getCourseByUserId", e);
+		}
+		return null;
+	}
 
-    @RequestMapping(value = "/getCourseByCourseIds", method = RequestMethod.GET)
-    @ApiOperation(value = "根据课程id获取课程数据", notes = "根据课程id获取课程数据")
-    @ApiImplicitParams({//请求参数描述
-            @ApiImplicitParam(name = "id", value = "课程ID", required = true, dataTypeClass = Long.class),
-    })
-    public GogoupCourse getCourseByCourseIds() {
-        GogoupCourse user = courseService.getCourseByUserId(Long.valueOf(4));
-        return user;
-    }
+	@RequestMapping(value = "/getCourseByCourseIds", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+	@ApiOperation(value = "根据课程id获取课程数据", notes = "根据课程id获取课程数据")
+	@ApiImplicitParams({ // 请求参数描述
+			@ApiImplicitParam(name = "id", value = "课程ID", required = true, dataTypeClass = Long.class), })
+	public GogoupCourse getCourseByCourseIds() {
+		GogoupCourse user = courseService.getCourseByUserId(Long.valueOf(4));
+		return user;
+	}
 }
